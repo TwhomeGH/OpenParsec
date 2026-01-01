88afa488efa7d0a4d7d08777f83501a5cfaba7e3
Nuclear0709 <42139132+TwhomeGH@users.noreply.github.com>
2026-01-02 07:43:55 +0800

    Clean up unused code in ParsecGLKViewController
    Removed commented-out SwiftUI and GLKit code for ParsecGLKViewController.

--- OpenParsec/ParsecGLKViewController.swift
+++ OpenParsec/ParsecGLKViewController.swift
@@ -1,28 +1,3 @@
-//import SwiftUI
-//import GLKit
-//
-//struct ParsecGLKViewController:UIViewControllerRepresentable
-//{
-//	let glkView = GLKView()
-//	let glkViewController = GLKViewController()
-//	let onBeforeRender:() -> Void
-//
-//	func makeCoordinator() -> ParsecGLKRenderer
-//	{
-//		ParsecGLKRenderer(glkView, glkViewController, onBeforeRender)
-//	}
-//
-//	func makeUIViewController(context:UIViewControllerRepresentableContext<ParsecGLKViewController>) -> GLKViewController
-//	{
-//		glkView.context = EAGLContext(api:.openGLES3)!
-//		glkViewController.view = glkView
-//		glkViewController.preferredFramesPerSecond = 60
-//		return glkViewController
-//	}
-//
-//	func updateUIViewController(_ uiViewController:GLKViewController, context:UIViewControllerRepresentableContext<ParsecGLKViewController>) { }
-//}
-
 import UIKit
 import GLKit
 

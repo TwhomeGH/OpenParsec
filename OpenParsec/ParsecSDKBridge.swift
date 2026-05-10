ed728a84aec7146686dc02b66f898c9d8741bdec
Nuclear0709 <agp05432@gmail.com>
2026-05-10 11:45:36 +0800

    修正類型Int32

--- OpenParsec/ParsecSDKBridge.swift
+++ OpenParsec/ParsecSDKBridge.swift
@@ -113,8 +113,8 @@ class ParsecSDKBridge: ParsecService
 
 		var parsecClientCfg = ParsecClientConfig()
 		parsecClientCfg.video.0.decoderIndex = 1
-		parsecClientCfg.video.0.resolutionX = SettingsHandler.shared.resolution.width
-		parsecClientCfg.video.0.resolutionY = SettingsHandler.shared.resolution.height
+		parsecClientCfg.video.0.resolutionX = Int32(SettingsHandler.shared.resolution.width)
+		parsecClientCfg.video.0.resolutionY = Int32(SettingsHandler.shared.resolution.height)
 
 		parsecClientCfg.video.0.decoderCompatibility = SettingsHandler.shared.decoderCompatibility
 		parsecClientCfg.video.0.decoder444 = SettingsHandler.shared.decoder444

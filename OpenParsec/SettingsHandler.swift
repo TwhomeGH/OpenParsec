c1c58052715074d39319e72e3404e9f0da79f189
Nuclear0709 <agp05432@gmail.com>
2026-05-10 11:01:06 +0800

    調整測試

--- OpenParsec/SettingsHandler.swift
+++ OpenParsec/SettingsHandler.swift
@@ -1,7 +1,7 @@
 import Foundation
 import SwiftUI
 
-final class SettingsHandler: ObservableObject {
+final class SettingsHandler {
 	// 全局配置中軀
 	static let shared = SettingsHandler()   // 單例，全局共用
 

5505bf51ffe0e5738fc2dd473b99ee8416126d45
Nuclear0709 <agp05432@gmail.com>
2026-05-10 10:40:50 +0800

    從Struc轉為Class作為全局配置組件

--- OpenParsec/SettingsHandler.swift
+++ OpenParsec/SettingsHandler.swift
@@ -1,8 +1,9 @@
 import Foundation
 import SwiftUI
 
-struct SettingsHandler {
-
+final class SettingsHandler {
+	// 全局配置中軀
+	
 	@AppStorage("renderer") public static var renderer: RendererType = .opengl
 
 	@AppStorage("resolution") public static var resolution: ParsecResolution = .client

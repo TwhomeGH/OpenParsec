063c6eabc0552bae1b1413090859bb7f6ce6d780
Nuclear0709 <agp05432@gmail.com>
2026-05-11 09:24:14 +0800

    調整glkview

--- OpenParsec/ParsecGLKRenderer.swift
+++ OpenParsec/ParsecGLKRenderer.swift
@@ -32,47 +32,40 @@ class ParsecGLKRenderer:NSObject, GLKViewDelegate, GLKViewControllerDelegate
 		glkViewController.delegate = nil
 	}
 
-	func glkView(_ view:GLKView, drawIn rect:CGRect)
-	{
-
-		// 用 drawableWidth/Height 而不是 bounds
-		let width = CGFloat(view.drawableWidth)
-		let height = CGFloat(view.drawableHeight)
+	func glkView(_ view: GLKView, drawIn rect: CGRect) {
+		let width = view.bounds.size.width
+		let height = view.bounds.size.height
 		let scale = view.contentScaleFactor
-
 
 		let deltaWidth = abs(width - lastWidth)
 		let deltaHeight = abs(height - lastHeight)
 		let deltaScale = abs(scale - lastScale)
 
-		if deltaWidth > 0.1 || deltaHeight > 0.1 || deltaScale > 0.001
-		{
-		    CParsec.setFrame(width, height, scale)
+		if deltaWidth > 0.1 || deltaHeight > 0.1 || deltaScale > 0.001 {
+			// 用邏輯大小 + scale 告訴 Parsec
+			CParsec.setFrame(width, height, scale)
 
-			// 確保 OpenGL viewport 和 GLKView 對齊
-			glViewport(0, 0, GLsizei(width), GLsizei(height))
-			
-	        lastWidth = width
+			// 用像素大小設置 OpenGL viewport
+			glViewport(0, 0,
+					GLsizei(view.drawableWidth),
+					GLsizei(view.drawableHeight))
+
+			lastWidth = width
 			lastHeight = height
 			lastScale = scale
 			print("SCALE", scale)
 			print("Width:\(width)x\(height)")
 		}
 
-		// Calculate timeout based on configured/device frame rate
-		// timeout in ms: 16ms = ~60fps, 8ms = ~120fps
 		let fps = SettingsHandler.shared.preferredFramesPerSecond == 0
 			? UIScreen.main.maximumFramesPerSecond
 			: SettingsHandler.shared.preferredFramesPerSecond
-		let timeout = UInt32(max(1000 / fps, 8)) // minimum 8ms for 120Hz
+		let timeout = UInt32(max(1000 / fps, 8))
 
 		CParsec.renderGLFrame(timeout: timeout)
-
 		updateImage()
-
-//		glFinish()
-		//glFlush()
 	}
+
 
 	func glkViewControllerUpdate(_ controller:GLKViewController) { }
 
